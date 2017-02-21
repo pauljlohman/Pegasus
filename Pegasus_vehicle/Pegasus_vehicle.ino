8cff8574900d43a94035df1fa94922d2580ac638
Paul Lohman <pauljlohman@hotmail.com>
2017-02-20 21:47:36 -0500

    revised magnetometer
    Made several modifications to how the magnetometer is used.
    It is still disabled though.

--- Pegasus_vehicle/Pegasus_vehicle.ino
+++ Pegasus_vehicle/Pegasus_vehicle.ino
@@ -12,6 +12,7 @@
 
 #include "GyroAngle.h"
 #include "AccelAngle.h"
+#include "CompassAngle.h"
 #include "XRotor.h"
 #include "pPID.h"
 #include "CurveRemap.h"
@@ -42,6 +43,7 @@ byte connectionLostCount = 0;
 MPU9255 imu;
 GyroAngle gyro;
 AccelAngle accel;
+CompassAngle compass;
 // raw sensor readings
 short ax, ay, az, gx, gy, gz, mx, my, mz;
 
@@ -144,7 +146,7 @@ void setup() {
     esc.init(motorPin_FR);
     esc.init(motorPin_BR);
     esc.init(motorPin_BL);
-    calibrateESC();
+    esc_calibrate();
     print("ESC Setup Complete\n");
 
     // RX initialize
@@ -161,8 +163,7 @@ void setup() {
     radio.startRX();
     print("RX Setup Complete\n");
 
-    // pid.config(float p, float i, float d, float outputMin, float outputMax)
-    pidConfig();
+    pid_config();
 
     //GyroAngle.config( ushort FullScaleRangeDPS,
     //                  ulong deltaTime_UnitsPerSecond,
@@ -170,7 +171,8 @@ void setup() {
     //                  bool invX, invY, invZ)
     gyro.config(2000, 1000000, 0.01, false, true, true);
     accel.config(false, true, false);//args are for inverting axis
-
+    compass.config(152.0, 197.5, -172.5, 1.076132, 1.039761, 1.0); // see compass_calibrate()
+  
     // IMU initialize
     print("IMU Setup...");
     imu.initialize();
@@ -184,11 +186,9 @@ void setup() {
         while (true) {}
     }
     print("IMU Setup Complete\n");
-
-    print("Gyro Calibration...");
-    calibrateIMU();
-    print("Gyro Calibration Complete\n");
-
+    imu_calibrate();    
+    //compass_calibrate();
+    
     // clear log
     //print("Clearing Log...");
     //logger.clear();
@@ -201,7 +201,7 @@ void setup() {
     while(ctrl.control_code != ctrl.NAV_CODE){
         if (millis() - lastMidTime > midPeriod) {
             lastMidTime = millis();
-            checkRX();
+            rx_check();
         }
     }
     print("Connected\n");
@@ -221,13 +221,13 @@ void loop() {
   elapsed_ms = millis() - lastLongTime;
   if (elapsed_ms >= longPeriod) {
     lastLongTime = millis();
-    checkConnection();
+    rx_watchdog();
   }
 
   elapsed_ms = millis() - lastMidTime;
   if (elapsed_ms >= midPeriod) {
     lastMidTime = millis();
-    if (checkRX()) {
+    if (rx_check()) {
       validPacketCount++;
     }
   }
@@ -237,17 +237,27 @@ void loop() {
     lastShortTime = micros();
     lastShortTimeMS = millis();
     imu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-    //bool freshMag = imu.getMagReading( &mx, &my, &mz);
+    //bool updatedMag = imu.getMagReading( &mx, &my, &mz);
     gyro.update(gx, gy, gz, elapsed_si);
     //print("Rate "); print(lastShortTimeMS); print(" "); print(gyro.rateX); print(" "); print(gyro.rateY); print(" "); print(gyro.rateZ); print("\n");
     //print("Gyro "); print(lastShortTimeMS); print(" "); print(gyro.accX); print(" "); print(gyro.accY); print(" "); print(gyro.accZ); print("\n");
     accel.update(ax, ay, az);
     //print("Accel "); print(lastShortTimeMS); print(" "); print(accel.x); print(" "); print(accel.y); print(" "); print(accel.z); print("\n");
-    //print("Compass "); print(lastShortTimeMS); print(" ");print(accel.mx); print(" "); print(accel.y); print(" "); print(accel.z); print("\n");
     gyro.correctXY(accel.x, accel.y);
-    //if(freshMag){ gyro.correctZ(accel.z); }
+    /*
+    if(updatedMag){ 
+        compass.update(mx, my, mz, ax, ay, az);
+        //print("Compass "); print(lastShortTimeMS); print(" ");print(compass.mx); print(" "); print(compass.my); print(" "); print(compass.mz); print("\n");
+        gyro.correctZ(compass.yaw); 
+    }
+    */
     //print("IMU "); print(lastShortTimeMS); print(" "); print(gyro.posX); print(" "); print(gyro.posY); print(" "); print(gyro.posZ); print("\n");
-    updateMotor();
+    if (ctrl.live) {
+        pid_update();
+        motor_update();
+    }else{
+        motor_off();
+    }
   }
 
   elapsed_ms = millis() - lastloggerTime;
@@ -266,7 +276,7 @@ void loop() {
   }
 }
 
-bool checkRX() {
+bool rx_check() {
   byte packetCount = radio.checkReceived(buf); // check for new rx data
   if (packetCount) {
     // get last packet received
@@ -291,15 +301,15 @@ bool checkRX() {
         return true;
       case ctrl.PID_YAW_CODE: // PID Yaw settings
         print("Pid_Yaw_Settings "); print(ctrl.Kp_yaw); print(" "); print(ctrl.Ki_yaw); print(" "); print(ctrl.Kd_yaw); print("\n");
-        pidConfig();
+        pid_config();
         return true;
       case ctrl.PID_PITCH_CODE: // PID Pitch settings
         print("Pid_Pitch_Settings "); print(ctrl.Kp_pitch); print(" "); print(ctrl.Ki_pitch); print(" "); print(ctrl.Kd_pitch); print("\n");
-        pidConfig();
+        pid_config();
         return true;
       case ctrl.PID_ROLL_CODE: // PID Roll settings
         print("Pid_Roll_Settings "); print(ctrl.Kp_roll); print(" "); print(ctrl.Ki_roll); print(" "); print(ctrl.Kd_roll); print("\n");
-        pidConfig();
+        pid_config();
         return true;
       default: // unknown control code
         print("UNKNOWN ");
@@ -314,7 +324,31 @@ bool checkRX() {
   return false;
 }
 
-void updateMotor() {
+void rx_watchdog() {
+  // checked every 500ms
+  if (validPacketCount < 10) { // should be ~22 per 500ms
+    // set nav control to hover
+    ctrl.yawf = 0.0;
+    ctrl.pitchf = 0.0;
+    ctrl.rollf = 0.0;
+    ctrl.throttle = 0.0;
+    connectionLostCount++;
+    digitalWrite(statPin_g, LOW);
+    digitalWrite(statPin_r, HIGH);
+    print("Got fewer packets then expected\n");
+  } else {
+    connectionLostCount = 0;
+    digitalWrite(statPin_g, HIGH);
+    digitalWrite(statPin_r, LOW);
+  }
+  validPacketCount = 0;
+  if(connectionLostCount > 6){ // shut down after 3 seconds of lost connection
+    ctrl.live = false;
+    print("Lost connection, shutdown\n");
+  }
+}
+
+void pid_update() {
   // update PID & scale to power
   //print("PID_R "); print(lastShortTimeMS); print(" "); print(ctrl.rollf); print(" ");
   switch (ctrl.navRotModeRoll) {
@@ -363,13 +397,22 @@ void updateMotor() {
   //print(PID_yaw.getIntegral()); print(" "); print(ctrl.Ki_yaw); print(" "); print(ctrl.Ki_yaw * PID_yaw.getIntegral()); print(" ")
   //print(PID_yaw.getDerivative()); print(" "); print(ctrl.Kd_yaw); print(" "); print(ctrl.Kd_yaw * PID_yaw.getDerivative()); print(" ")
   //print(op_yaw); print("\n");
+}
 
-  op_throttle = ctrl.throttle;
-  
+void pid_config() {
+  // pid.config(float p, float i, float d, float outputMin, float outputMax)
+  PID_yaw.config(shortPeriod, ctrl.Kp_yaw, ctrl.Ki_yaw, ctrl.Kd_yaw, -65535.0, 65535.0);
+  PID_pitch.config(shortPeriod, ctrl.Kp_pitch, ctrl.Ki_pitch, ctrl.Kd_pitch, -65535.0, 65535.0);
+  PID_roll.config(shortPeriod, ctrl.Kp_roll, ctrl.Ki_roll, ctrl.Kd_roll, -65535.0, 65535.0);
+}
+
+void motor_update() {  
   //    roll
   // fl-----fr
   // |  yaw  | pitch
   // bl-----br
+
+  op_throttle = ctrl.throttle;
   power_FL = op_throttle + op_pitch + op_roll - op_yaw;
   power_FR = op_throttle + op_pitch - op_roll + op_yaw;
   power_BR = op_throttle - op_pitch - op_roll - op_yaw;
@@ -380,55 +423,25 @@ void updateMotor() {
   power_BR = tt.interp(power_BR, BR_tt_x, BR_tt_y, BRTTLENGTH);
   power_BL = tt.interp(power_BL, BL_tt_x, BL_tt_y, BLTTLENGTH);
   */
-    
+  
   //print("MP "); print(lastShortTimeMS); print(" "); print(power_FL); print(" "); print(power_FR); print(" "); print(power_BR); print(" "); print(power_BL); print("\n");
-    
-  if (ctrl.live) {
-    esc.update(motorPin_FL, power_FL);
-    esc.update(motorPin_FR, power_FR);
-    esc.update(motorPin_BR, power_BR);
-    esc.update(motorPin_BL, power_BL);
-  } else {
+
+  esc.update(motorPin_FL, power_FL);
+  esc.update(motorPin_FR, power_FR);
+  esc.update(motorPin_BR, power_BR);
+  esc.update(motorPin_BL, power_BL);
+}
+
+void motor_off() {
     esc.update(motorPin_FL, 0);
     esc.update(motorPin_FR, 0);
     esc.update(motorPin_BR, 0);
     esc.update(motorPin_BL, 0);
-  }
-
 }
 
-void checkConnection() {
-  // checked every 500ms
-  if (validPacketCount < 10) { // should be ~22 per 500ms
-    // set nav control to hover
-    ctrl.yawf = 0.0;
-    ctrl.pitchf = 0.0;
-    ctrl.rollf = 0.0;
-    ctrl.throttle = 0.0;
-    connectionLostCount++;
-    digitalWrite(statPin_g, LOW);
-    digitalWrite(statPin_r, HIGH);
-    print("Got fewer packets then expected\n");
-  } else {
-    connectionLostCount = 0;
-    digitalWrite(statPin_g, HIGH);
-    digitalWrite(statPin_r, LOW);
-  }
-  validPacketCount = 0;
-  if(connectionLostCount > 6){ // shut down after 3 seconds of lost connection
-    ctrl.live = false;
-    print("Lost connection, shutdown\n");
-  }
-}
-
-void pidConfig() {
-  PID_yaw.config(shortPeriod, ctrl.Kp_yaw, ctrl.Ki_yaw, ctrl.Kd_yaw, -65535.0, 65535.0);
-  PID_pitch.config(shortPeriod, ctrl.Kp_pitch, ctrl.Ki_pitch, ctrl.Kd_pitch, -65535.0, 65535.0);
-  PID_roll.config(shortPeriod, ctrl.Kp_roll, ctrl.Ki_roll, ctrl.Kd_roll, -65535.0, 65535.0);
-}
-
-void calibrateIMU() {
-  Serial.println();
+void imu_calibrate() {
+  print("IMU Calibration...\n");
+  print("Sensor should be at rest\n");
   // gyro offset values
   long gx_sum = 0; long gy_sum = 0; long gz_sum = 0; // sum
   float gx_avg = 0; float gy_avg = 0; float gz_avg = 0; // average
@@ -438,13 +451,14 @@ void calibrateIMU() {
   long ax_sum = 0; long ay_sum = 0; long az_sum = 0; // sum
   float ax_avg = 0; float ay_avg = 0; float az_avg = 0; // average
   float ax_pavg = 0; float ay_pavg = 0; float az_pavg = 0; // previous average
-  // heading from compass
+  
+  // vector from compass
   long mx_sum = 0; long my_sum = 0; long mz_sum = 0; // sum
   float mx_avg = 0; float my_avg = 0; float mz_avg = 0; // average
   float mx_pavg = 0; float my_pavg = 0; float mz_pavg = 0; // previous average
 
   bool stable = false;
-  float g_epsilon = 0.015;
+  float g_epsilon = 0.01;
   float a_epsilon = 0.1;
   float m_epsilon = 0.01;
   float count = 0.0;
@@ -458,7 +472,7 @@ void calibrateIMU() {
     gx_avg = gx_sum / count; gy_avg = gy_sum / count; gz_avg = gz_sum / count;
     ax_avg = ax_sum / count; ay_avg = ay_sum / count; az_avg = az_sum / count;
     mx_avg = mx_sum / count; my_avg = my_sum / count; mz_avg = mz_sum / count;
-    if(int(count) > 10){
+    if(int(count) > 20){
         if(fabs(gx_avg-gx_pavg) > g_epsilon){ stable = false;}
         if(fabs(gy_avg-gy_pavg) > g_epsilon){ stable = false;}
         if(fabs(gz_avg-gz_pavg) > g_epsilon){ stable = false;}
@@ -476,19 +490,73 @@ void calibrateIMU() {
     mx_pavg = mx_avg; my_pavg = my_avg; mz_pavg = mz_avg;
     delay(5);
   }while(!stable);
-
+  
+  compass.update(int(mx_avg), int(my_avg), int(mz_avg), int(ax_avg), int(ay_avg), int(az_avg));
+  compass.init(compass.yaw);
   accel.setRestingPosition(0.0, 0.0, 1.0);
-  //accel.setRestingPosition(233.5522003, 255.8198547, 8202.2744141);
+  //accel.setRestingPosition(int(ax_avg), int(ay_avg), int(az_avg)); // only use if craft is perfectly level
   accel.update(int(ax_avg), int(ay_avg), int(az_avg));
-  gyro.init(gx_avg, gy_avg, gz_avg, accel.x, accel.y, accel.z); // gyro offset xyz, accel starting rotation xyz
+  gyro.init(gx_avg, gy_avg, gz_avg, accel.x, accel.y, 0.0); // gyro offset xyz, accel starting rotation xyz
   #ifdef DEBUG
-    Serial.printf("\tresting accel vector %0.3f %0.3f %0.3f \n", ax_avg, ay_avg, az_avg);
-    Serial.printf("\tcurrent accel angle %0.3f %0.3f %0.3f \n", accel.x, accel.y, accel.z);
-    Serial.printf("\tgyro offset %0.3f %0.3f %0.3f \n", gx_avg, gy_avg, gz_avg);
+    Serial.printf("accel resting vector %0.3f %0.3f %0.3f\n", ax_avg, ay_avg, az_avg);
+    Serial.printf("accel current angle %0.3f %0.3f %0.3f\n", accel.x, accel.y, accel.z);
+    Serial.printf("gyro rate offset %0.3f %0.3f %0.3f\n", gx_avg, gy_avg, gz_avg);
+    Serial.printf("compass resting heading %0.3f\n", compass.yaw);
   #endif
+  print("IMU Calibration Complete\n");
 }
 
-void calibrateESC(){
+void compass_calibrate(){
+  print("Compass Calibration...\n");
+  print("move sensor around in all orientations, monitor the min/max values, run until satisfied\n");
+  print("use results to set compass.config(Xoffset,Yoffset,Zoffset,Xscale,Yscale,Zscale);");
+  delay(2000);
+  float Xoffset, Yoffset, Zoffset;
+  float Xscale, Yscale, Zscale;
+  float longCord;
+  int16_t Xmax = -32760; 
+  int16_t Xmin = 32760;
+  int16_t Ymax = -32760; 
+  int16_t Ymin = 32760;
+  int16_t Zmax = -32760; 
+  int16_t Zmin = 32760;
+  while(true){
+    while(!imu.getMagReading( &mx, &my, &mz)){}
+    Xmax = mx > Xmax ? mx : Xmax;
+    Xmin = mx < Xmin ? mx : Xmin;
+    Ymax = my > Ymax ? my : Ymax;
+    Ymin = my < Ymin ? my : Ymin;
+    Zmax = mz > Zmax ? mz : Zmax;
+    Zmin = mz < Zmin ? mz : Zmin;
+    
+    Xoffset = (Xmax + Xmin)/2.0;
+    Yoffset = (Ymax + Ymin)/2.0;
+    Zoffset = (Zmax + Zmin)/2.0;
+
+    // find scale from largest chord length
+    longCord = float(max( max(Xmax - Xmin, Ymax - Ymin), Zmax - Zmin));
+    Xscale = longCord/(Xmax - Xmin);
+    Yscale = longCord/(Ymax - Ymin);
+    Zscale = longCord/(Zmax - Zmin);
+
+    #ifdef DEBUG
+        Serial.printf("X min %i max %i = offset %0.6f scale %0.6f\n", Xmin, Xmax, Xoffset, Xscale);
+        Serial.printf("Y min %i max %i = offset %0.6f scale %0.6f\n", Ymin, Ymax, Yoffset, Yscale);
+        Serial.printf("Z min %i max %i = offset %0.6f scale %0.6f\n", Zmin, Zmax, Zoffset, Zscale);
+    #endif
+  }
+  // use results to set compass.config(Xoffset,Yoffset,Zoffset,Xscale,Yscale,Zscale);
+  /*
+  // alternatively use scale from factory settings, see data sheet
+  int8_t rom_adjX, rom_adjY, rom_adjZ;
+  imu.getMagAdjustment(&rom_adjX, &rom_adjY, &rom_adjZ);
+  float Xscale = (((int(rom_adjX)-128)*0.5)/128.0)+1;
+  float Yscale = (((int(rom_adjY)-128)*0.5)/128.0)+1;
+  float Zscale = (((int(rom_adjZ)-128)*0.5)/128.0)+1;
+  */  
+}
+
+void esc_calibrate() {
     esc.update(motorPin_FL, throttleMax);
     esc.update(motorPin_FR, throttleMax);
     esc.update(motorPin_BR, throttleMax);

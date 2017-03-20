19c3ce6544ba246d4879f40c624a7ec86dcf5df4
Paul Lohman <pauljlohman@hotmail.com>
2017-03-19 23:16:40 -0400

    Major revision
    -added throttle range and mode storage
    -dropped the different PID settings per nav modes

--- libraries/Pegasus_CtrlData/Pegasus_ConfigMem.h
+++ libraries/Pegasus_CtrlData/Pegasus_ConfigMem.h
@@ -1,23 +1,20 @@
 #include "EEPROM_I2C.h"
 
 // ADDRESS
-// NAV ROTATION MODE 3 bytes
-//+0 yaw, +1 pitch, +2 roll
-#define ADDR_ROT_MODE 0
+// NAV MODE 4 bytes
+//+0 yaw, +1 pitch, +2 roll, +3 throttle
+#define ADDR_NAV_MODE 0
 
-// NAV ROTATION RANGE 3 bytes
-//+0 yaw, +1 pitch, +2 roll
-#define ADDR_ROT_RANGE_ACRO 3
-#define ADDR_ROT_RANGE_STAB 6
+// NAV RANGE 5 bytes
+//+0 yaw, +1 pitch, +2 roll, +3 throttle
+#define ADDR_NAV_RANGE_REL 4
+#define ADDR_NAV_RANGE_ABS 9
 
 // PID 6 bytes, 3 uint16
 //+0 upper P, +1 lower P, +2 upper I, +3 lower I, +4 upper D, +5 lower D
-#define ADDR_PID_YAW_ACRO 12
-#define ADDR_PID_YAW_STAB 18
-#define ADDR_PID_PITCH_ACRO 24
-#define ADDR_PID_PITCH_STAB 30
-#define ADDR_PID_ROLL_ACRO 36
-#define ADDR_PID_ROLL_STAB 42
+#define ADDR_PID_YAW 14
+#define ADDR_PID_PITCH 20
+#define ADDR_PID_ROLL 26
 
 #ifndef PEGASUS_CONFIGMEM_H
 #define PEGASUS_CONFIGMEM_H
@@ -29,291 +26,220 @@ public:
     void init(){
         mem.devAddr = B1010000;
     };
-    // write Rotation Mode
-    void write_RotMode_Yaw(byte mode){
-        mem.writeByte(ADDR_ROT_MODE+0, mode);
+    
+    // write Nav Mode
+    void write_NavMode_Yaw(byte mode){
+        mem.writeByte(ADDR_NAV_MODE+0, mode);
     };
-    void write_RotMode_Pitch(byte mode){
-        mem.writeByte(ADDR_ROT_MODE+1, mode);
+    void write_NavMode_Pitch(byte mode){
+        mem.writeByte(ADDR_NAV_MODE+1, mode);
     };
-    void write_RotMode_Roll(byte mode){
-        mem.writeByte(ADDR_ROT_MODE+2, mode);
+    void write_NavMode_Roll(byte mode){
+        mem.writeByte(ADDR_NAV_MODE+2, mode);
     };
-    // write Rotation Range Acro
-    void write_RotRange_Yaw_Acro(byte range){
-        mem.writeByte(ADDR_ROT_RANGE_ACRO+0, range);
-    };
-    void write_RotRange_Pitch_Acro(byte range){
-        mem.writeByte(ADDR_ROT_RANGE_ACRO+1, range);
-    };
-    void write_RotRange_Roll_Acro(byte range){
-        mem.writeByte(ADDR_ROT_RANGE_ACRO+2, range);
-    };
-
-    // write Rotation Range Stab
-    void write_RotRange_Yaw_Stab(byte range){
-        mem.writeByte(ADDR_ROT_RANGE_STAB+0, range);
-    };
-    void write_RotRange_Pitch_Stab(byte range){
-        mem.writeByte(ADDR_ROT_RANGE_STAB+1, range);
-    };
-    void write_RotRange_Roll_Stab(byte range){
-        mem.writeByte(ADDR_ROT_RANGE_STAB+2, range);
-    };
-
-    // write PID Yaw Acro
-    void write_PID_Yaw_P_Acro(uint16_t v){
-        buf[0] = byte(v >> 8);
-        buf[1] = byte(v);
-        mem.write(ADDR_PID_YAW_ACRO+0, 2, buf);
-    };
-    void write_PID_Yaw_I_Acro(uint16_t v){
-        buf[0] = byte(v >> 8);
-        buf[1] = byte(v);
-        mem.write(ADDR_PID_YAW_ACRO+2, 2, buf);
-    };
-    void write_PID_Yaw_D_Acro(uint16_t v){
-        buf[0] = byte(v >> 8);
-        buf[1] = byte(v);
-        mem.write(ADDR_PID_YAW_ACRO+4, 2, buf);
-    };
-    // write PID Pitch Acro
-    void write_PID_Pitch_P_Acro(uint16_t v){
-        buf[0] = byte(v >> 8);
-        buf[1] = byte(v);
-        mem.write(ADDR_PID_PITCH_ACRO+0, 2, buf);
-    };
-    void write_PID_Pitch_I_Acro(uint16_t v){
-        buf[0] = byte(v >> 8);
-        buf[1] = byte(v);
-        mem.write(ADDR_PID_PITCH_ACRO+2, 2, buf);
-    };
-    void write_PID_Pitch_D_Acro(uint16_t v){
-        buf[0] = byte(v >> 8);
-        buf[1] = byte(v);
-        mem.write(ADDR_PID_PITCH_ACRO+4, 2, buf);
-    };
-    // write PID Roll Acro
-    void write_PID_Roll_P_Acro(uint16_t v){
-        buf[0] = byte(v >> 8);
-        buf[1] = byte(v);
-        mem.write(ADDR_PID_ROLL_ACRO+0, 2, buf);
-    };
-    void write_PID_Roll_I_Acro(uint16_t v){
-        buf[0] = byte(v >> 8);
-        buf[1] = byte(v);
-        mem.write(ADDR_PID_ROLL_ACRO+2, 2, buf);
-    };
-    void write_PID_Roll_D_Acro(uint16_t v){
-        buf[0] = byte(v >> 8);
-        buf[1] = byte(v);
-        mem.write(ADDR_PID_ROLL_ACRO+4, 2, buf);
-    };
-
-    // write PID Yaw Stab
-    void write_PID_Yaw_P_Stab(uint16_t v){
-        buf[0] = byte(v >> 8);
-        buf[1] = byte(v);
-        mem.write(ADDR_PID_YAW_STAB+0, 2, buf);
-    };
-    void write_PID_Yaw_I_Stab(uint16_t v){
-        buf[0] = byte(v >> 8);
-        buf[1] = byte(v);
-        mem.write(ADDR_PID_YAW_STAB+2, 2, buf);
-    };
-    void write_PID_Yaw_D_Stab(uint16_t v){
-        buf[0] = byte(v >> 8);
-        buf[1] = byte(v);
-        mem.write(ADDR_PID_YAW_STAB+4, 2, buf);
-    };
-    // write PID Pitch Stab
-    void write_PID_Pitch_P_Stab(uint16_t v){
-        buf[0] = byte(v >> 8);
-        buf[1] = byte(v);
-        mem.write(ADDR_PID_PITCH_STAB+0, 2, buf);
-    };
-    void write_PID_Pitch_I_Stab(uint16_t v){
-        buf[0] = byte(v >> 8);
-        buf[1] = byte(v);
-        mem.write(ADDR_PID_PITCH_STAB+2, 2, buf);
-    };
-    void write_PID_Pitch_D_Stab(uint16_t v){
-        buf[0] = byte(v >> 8);
-        buf[1] = byte(v);
-        mem.write(ADDR_PID_PITCH_STAB+4, 2, buf);
-    };
-    // write PID Roll Stab
-    void write_PID_Roll_P_Stab(uint16_t v){
-        buf[0] = byte(v >> 8);
-        buf[1] = byte(v);
-        mem.write(ADDR_PID_ROLL_STAB+0, 2, buf);
-    };
-    void write_PID_Roll_I_Stab(uint16_t v){
-        buf[0] = byte(v >> 8);
-        buf[1] = byte(v);
-        mem.write(ADDR_PID_ROLL_STAB+2, 2, buf);
-    };
-    void write_PID_Roll_D_Stab(uint16_t v){
-        buf[0] = byte(v >> 8);
-        buf[1] = byte(v);
-        mem.write(ADDR_PID_ROLL_STAB+4, 2, buf);
-    };
-
-
-    // read Rotation Mode
-    byte read_RotMode_Yaw(){
-        return mem.readByte(ADDR_ROT_MODE+0);
-    };
-    byte read_RotMode_Pitch(){
-        return mem.readByte(ADDR_ROT_MODE+1);
-    };
-    byte read_RotMode_Roll(){
-        return mem.readByte(ADDR_ROT_MODE+2);
+    void write_NavMode_Throttle(byte mode){
+        mem.writeByte(ADDR_NAV_MODE+3, mode);
     };
     
-    // read Rotation Range Acro
-    byte read_RotRange_Yaw_Acro(){
-        return mem.readByte(ADDR_ROT_RANGE_ACRO+0);
+    // write Nav Range Relative
+    void write_NavRange_Yaw_Rel(byte range){
+        mem.writeByte(ADDR_NAV_RANGE_REL+0, range);
     };
-    byte read_RotRange_Pitch_Acro(){
-        return mem.readByte(ADDR_ROT_RANGE_ACRO+1);
+    void write_NavRange_Pitch_Rel(byte range){
+        mem.writeByte(ADDR_NAV_RANGE_REL+1, range);
     };
-    byte read_RotRange_Roll_Acro(){
-        return mem.readByte(ADDR_ROT_RANGE_ACRO+2);
+    void write_NavRange_Roll_Rel(byte range){
+        mem.writeByte(ADDR_NAV_RANGE_REL+2, range);
+    };
+    void write_NavRange_Throttle_Rel(uint16_t range){
+        buf[0] = byte(range >> 8);
+        buf[1] = byte(range);
+        mem.write(ADDR_NAV_RANGE_REL+3, 2, buf);
+    };
+    
+    // write Nav Range Absolute
+    void write_NavRange_Yaw_Abs(byte range){
+        mem.writeByte(ADDR_NAV_RANGE_ABS+0, range);
+    };
+    void write_NavRange_Pitch_Abs(byte range){
+        mem.writeByte(ADDR_NAV_RANGE_ABS+1, range);
+    };
+    void write_NavRange_Roll_Abs(byte range){
+        mem.writeByte(ADDR_NAV_RANGE_ABS+2, range);
+    };
+    void write_NavRange_Throttle_Abs(uint16_t range){
+        buf[0] = byte(range >> 8);
+        buf[1] = byte(range);
+        mem.write(ADDR_NAV_RANGE_ABS+3, 2, buf);
+    };
+    
+    // write PID Yaw
+    void write_PID_Yaw_P(uint16_t v){
+        buf[0] = byte(v >> 8);
+        buf[1] = byte(v);
+        mem.write(ADDR_PID_YAW+0, 2, buf);
+    };
+    void write_PID_Yaw_I(uint16_t v){
+        buf[0] = byte(v >> 8);
+        buf[1] = byte(v);
+        mem.write(ADDR_PID_YAW+2, 2, buf);
+    };
+    void write_PID_Yaw_D(uint16_t v){
+        buf[0] = byte(v >> 8);
+        buf[1] = byte(v);
+        mem.write(ADDR_PID_YAW+4, 2, buf);
+    };
+    
+    // write PID Pitch
+    void write_PID_Pitch_P(uint16_t v){
+        buf[0] = byte(v >> 8);
+        buf[1] = byte(v);
+        mem.write(ADDR_PID_PITCH+0, 2, buf);
+    };
+    void write_PID_Pitch_I(uint16_t v){
+        buf[0] = byte(v >> 8);
+        buf[1] = byte(v);
+        mem.write(ADDR_PID_PITCH+2, 2, buf);
+    };
+    void write_PID_Pitch_D(uint16_t v){
+        buf[0] = byte(v >> 8);
+        buf[1] = byte(v);
+        mem.write(ADDR_PID_PITCH+4, 2, buf);
+    };
+    
+    // write PID Roll
+    void write_PID_Roll_P(uint16_t v){
+        buf[0] = byte(v >> 8);
+        buf[1] = byte(v);
+        mem.write(ADDR_PID_ROLL+0, 2, buf);
+    };
+    void write_PID_Roll_I(uint16_t v){
+        buf[0] = byte(v >> 8);
+        buf[1] = byte(v);
+        mem.write(ADDR_PID_ROLL+2, 2, buf);
+    };
+    void write_PID_Roll_D(uint16_t v){
+        buf[0] = byte(v >> 8);
+        buf[1] = byte(v);
+        mem.write(ADDR_PID_ROLL+4, 2, buf);
     };
 
-    // read Rotation Range Stab
-    byte read_RotRange_Yaw_Stab(){
-        return mem.readByte(ADDR_ROT_RANGE_STAB+0);
+    // read Nav Mode
+    byte read_NavMode_Yaw(){
+        return mem.readByte(ADDR_NAV_MODE+0);
     };
-    byte read_RotRange_Pitch_Stab(){
-        return mem.readByte(ADDR_ROT_RANGE_STAB+1);
+    byte read_NavMode_Pitch(){
+        return mem.readByte(ADDR_NAV_MODE+1);
     };
-    byte read_RotRange_Roll_Stab(){
-        return mem.readByte(ADDR_ROT_RANGE_STAB+2);
+    byte read_NavMode_Roll(){
+        return mem.readByte(ADDR_NAV_MODE+2);
     };
-
-    // read PID Yaw Acro
-    uint16_t read_PID_Yaw_P_Acro(){
-        mem.read(ADDR_PID_YAW_ACRO+0, 2, buf);
-        return ((uint16_t)buf[0]<<8) | buf[1];
+    byte read_NavMode_Throttle(){
+        return mem.readByte(ADDR_NAV_MODE+3);
     };
-    uint16_t read_PID_Yaw_I_Acro(){
-        mem.read(ADDR_PID_YAW_ACRO+2, 2, buf);
-        return ((uint16_t)buf[0]<<8) | buf[1];
+    
+    // read Nav Range Relative
+    byte read_NavRange_Yaw_Rel(){
+        return mem.readByte(ADDR_NAV_RANGE_REL+0);
     };
-    uint16_t read_PID_Yaw_D_Acro(){
-        mem.read(ADDR_PID_YAW_ACRO+4, 2, buf);
-        return ((uint16_t)buf[0]<<8) | buf[1];
+    byte read_NavRange_Pitch_Rel(){
+        return mem.readByte(ADDR_NAV_RANGE_REL+1);
     };
-    // read PID Pitch Acro
-    uint16_t read_PID_Pitch_P_Acro(){
-        mem.read(ADDR_PID_PITCH_ACRO+0, 2, buf);
-        return ((uint16_t)buf[0]<<8) | buf[1];
+    byte read_NavRange_Roll_Rel(){
+        return mem.readByte(ADDR_NAV_RANGE_REL+2);
     };
-    uint16_t read_PID_Pitch_I_Acro(){
-        mem.read(ADDR_PID_PITCH_ACRO+2, 2, buf);
-        return ((uint16_t)buf[0]<<8) | buf[1];
-    };
-    uint16_t read_PID_Pitch_D_Acro(){
-        mem.read(ADDR_PID_PITCH_ACRO+4, 2, buf);
-        return ((uint16_t)buf[0]<<8) | buf[1];
-    };
-    // read PID Roll Acro
-    uint16_t read_PID_Roll_P_Acro(){
-        mem.read(ADDR_PID_ROLL_ACRO+0, 2, buf);
-        return ((uint16_t)buf[0]<<8) | buf[1];
-    };
-    uint16_t read_PID_Roll_I_Acro(){
-        mem.read(ADDR_PID_ROLL_ACRO+2, 2, buf);
-        return ((uint16_t)buf[0]<<8) | buf[1];
-    };
-    uint16_t read_PID_Roll_D_Acro(){
-        mem.read(ADDR_PID_ROLL_ACRO+4, 2, buf);
+    uint16_t read_NavRange_Throttle_Rel(){
+        mem.read(ADDR_NAV_RANGE_REL+3, 2, buf);
         return ((uint16_t)buf[0]<<8) | buf[1];
     };
 
-    // read PID Yaw Stab
-    uint16_t read_PID_Yaw_P_Stab(){
-        mem.read(ADDR_PID_YAW_STAB+0, 2, buf);
+    // read Nav Range Absolute
+    byte read_NavRange_Yaw_Abs(){
+        return mem.readByte(ADDR_NAV_RANGE_ABS+0);
+    };
+    byte read_NavRange_Pitch_Abs(){
+        return mem.readByte(ADDR_NAV_RANGE_ABS+1);
+    };
+    byte read_NavRange_Roll_Abs(){
+        return mem.readByte(ADDR_NAV_RANGE_ABS+2);
+    };
+    uint16_t read_NavRange_Throttle_Abs(){
+        mem.read(ADDR_NAV_RANGE_ABS+3, 2, buf);
         return ((uint16_t)buf[0]<<8) | buf[1];
     };
-    uint16_t read_PID_Yaw_I_Stab(){
-        mem.read(ADDR_PID_YAW_STAB+2, 2, buf);
+    
+    // read PID Yaw Relative
+    uint16_t read_PID_Yaw_P(){
+        mem.read(ADDR_PID_YAW+0, 2, buf);
         return ((uint16_t)buf[0]<<8) | buf[1];
     };
-    uint16_t read_PID_Yaw_D_Stab(){
-        mem.read(ADDR_PID_YAW_STAB+4, 2, buf);
+    uint16_t read_PID_Yaw_I(){
+        mem.read(ADDR_PID_YAW+2, 2, buf);
         return ((uint16_t)buf[0]<<8) | buf[1];
     };
-    // read PID Pitch Stab
-    uint16_t read_PID_Pitch_P_Stab(){
-        mem.read(ADDR_PID_PITCH_STAB+0, 2, buf);
+    uint16_t read_PID_Yaw_D(){
+        mem.read(ADDR_PID_YAW+4, 2, buf);
         return ((uint16_t)buf[0]<<8) | buf[1];
     };
-    uint16_t read_PID_Pitch_I_Stab(){
-        mem.read(ADDR_PID_PITCH_STAB+2, 2, buf);
+    // read PID Pitch Relative
+    uint16_t read_PID_Pitch_P(){
+        mem.read(ADDR_PID_PITCH+0, 2, buf);
         return ((uint16_t)buf[0]<<8) | buf[1];
     };
-    uint16_t read_PID_Pitch_D_Stab(){
-        mem.read(ADDR_PID_PITCH_STAB+4, 2, buf);
+    uint16_t read_PID_Pitch_I(){
+        mem.read(ADDR_PID_PITCH+2, 2, buf);
         return ((uint16_t)buf[0]<<8) | buf[1];
     };
-    // read PID Roll Stab
-    uint16_t read_PID_Roll_P_Stab(){
-        mem.read(ADDR_PID_ROLL_STAB+0, 2, buf);
+    uint16_t read_PID_Pitch_D(){
+        mem.read(ADDR_PID_PITCH+4, 2, buf);
         return ((uint16_t)buf[0]<<8) | buf[1];
     };
-    uint16_t read_PID_Roll_I_Stab(){
-        mem.read(ADDR_PID_ROLL_STAB+2, 2, buf);
+    
+    // read PID Roll Relative
+    uint16_t read_PID_Roll_P(){
+        mem.read(ADDR_PID_ROLL+0, 2, buf);
         return ((uint16_t)buf[0]<<8) | buf[1];
     };
-    uint16_t read_PID_Roll_D_Stab(){
-        mem.read(ADDR_PID_ROLL_STAB+4, 2, buf);
+    uint16_t read_PID_Roll_I(){
+        mem.read(ADDR_PID_ROLL+2, 2, buf);
+        return ((uint16_t)buf[0]<<8) | buf[1];
+    };
+    uint16_t read_PID_Roll_D(){
+        mem.read(ADDR_PID_ROLL+4, 2, buf);
         return ((uint16_t)buf[0]<<8) | buf[1];
     };
 
     void format(){
-        // write Rotation Mode
-        write_RotMode_Yaw(1);
-        write_RotMode_Pitch(0);
-        write_RotMode_Roll(0);
+        // write Nav Mode
+        write_NavMode_Yaw(1);
+        write_NavMode_Pitch(0);
+        write_NavMode_Roll(0);
+        write_NavMode_Throttle(0);
 
-        // write Rotation Range Acro
-        write_RotRange_Yaw_Acro(2);
-        write_RotRange_Pitch_Acro(4);
-        write_RotRange_Roll_Acro(4);
+        // write Nav Range Relative
+        write_NavRange_Yaw_Rel(2);
+        write_NavRange_Pitch_Rel(4);
+        write_NavRange_Roll_Rel(4);
+        write_NavRange_Throttle_Rel(8);
 
-        // write Rotation Range Stab
-        write_RotRange_Yaw_Stab(15);
-        write_RotRange_Pitch_Stab(30);
-        write_RotRange_Roll_Stab(30);
+        // write Nav Range Absolute
+        write_NavRange_Yaw_Abs(15);
+        write_NavRange_Pitch_Abs(30);
+        write_NavRange_Roll_Abs(30);
+        write_NavRange_Throttle_Abs(32767);
+        
+        // write PID
+        write_PID_Yaw_P(100);
+        write_PID_Yaw_I(50);
+        write_PID_Yaw_D(30);
 
-        // write PID Acro
-        write_PID_Yaw_P_Acro(0);
-        write_PID_Yaw_I_Acro(0);
-        write_PID_Yaw_D_Acro(0);
+        write_PID_Pitch_P(120);
+        write_PID_Pitch_I(70);
+        write_PID_Pitch_D(50);
 
-        write_PID_Pitch_P_Acro(0);
-        write_PID_Pitch_I_Acro(0);
-        write_PID_Pitch_D_Acro(0);
-
-        write_PID_Roll_P_Acro(0);
-        write_PID_Roll_I_Acro(0);
-        write_PID_Roll_D_Acro(0);
-
-        // write PID Stab
-        write_PID_Yaw_P_Stab(0);
-        write_PID_Yaw_I_Stab(0);
-        write_PID_Yaw_D_Stab(0);
-
-        write_PID_Pitch_P_Stab(0);
-        write_PID_Pitch_I_Stab(0);
-        write_PID_Pitch_D_Stab(0);
-
-        write_PID_Roll_P_Stab(0);
-        write_PID_Roll_I_Stab(0);
-        write_PID_Roll_D_Stab(0);
+        write_PID_Roll_P(120);
+        write_PID_Roll_I(70);
+        write_PID_Roll_D(50);
     };
 };
 #endif // Pegasus_ConfigMem

b0ee57637a0b1c4b8aa5fbba4ad16686344b3a68
Paul Lohman <pauljlohman@hotmail.com>
2017-03-19 23:23:59 -0400

    Major revision
    -dropped two stick support, now meant to use a class instance per axis
    -added min/max range arg in config function

--- libraries/Stick/Stick.h
+++ libraries/Stick/Stick.h
@@ -7,87 +7,58 @@ http://www.third-helix.com/2013/04/12/doing-thumbstick-dead-zones-right.html
 #ifndef STICK_H
 #define STICK_H
 
-//#include <Dampen.h>
 #include <RollingAverage.h>
 
 class Stick {
-    byte pinX;
-    byte pinY;
-    //Dampen dampenX;
-    //Dampen dampenY;
-    RollingAverage avgX;
-    RollingAverage avgY;
-
+    byte pin;
+    RollingAverage avg;
+    float center = 512.0;
+    float outerEdge = 0.0;
+    float deadZone = 0.0;
+    float range = 512.0;
+    
     float mapf(float x, float in_min, float in_max, float out_min, float out_max){
       return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
     };
 
-    float vecLen(float x, float y){
-        return sqrt(x*x+y*y);
-    };
-
     public:
-        float centerX = 512.0;
-        float centerY = 512.0;
-        float outerEdge = 0.0;
-        float deadZone = 0.0;
-        float x, y;
+        float v;
 
-        void config(byte _pinX, byte _pinY, float _outerEdge, float _deadZone, float _dampen){
-            pinX = _pinX;
-            pinY = _pinY;
+        void config(byte _pin, float _outerEdge, float _deadZone, float _range){
+            pin = _pin;
             outerEdge = _outerEdge;
             deadZone = _deadZone;
-            //dampenX.config(_dampen);
-            //dampenY.config(_dampen);
-            avgX.config(4);
-            avgY.config(4);
+            range = _range;
+            avg.config(4);
         };
-        
+        void setRange(float _range){
+            range = _range;
+        };
         void calibrate(){
-            centerX = 0.0;
-            centerY = 0.0;
+            center = 0.0;
             for(int i=0; i<200; i++){
-                centerX += float(analogRead(pinX));
-                centerY += float(analogRead(pinY));
+                center += float(analogRead(pin));
                 delay(2);
             }
-            centerX /= 200.0;
-            centerY /= 200.0;
+            center /= 200.0;
         };
         
         void update(){
             //unsigned long time = millis();
-            
-            x = float(analogRead(pinX));
-            y = float(analogRead(pinY));
-            //Serial.printf("raw %f %f %i\n", x, y, time);
+            v = float(analogRead(pin));
+            //Serial.printf("raw %f\n", v, time);
+            v = avg.update(v);
+            //Serial.printf("damp %f %i\n", v, time);
 
-            //x = dampenX.update(x);
-            //y = dampenY.update(y);
-            x = avgX.update(x);
-            y = avgY.update(y);
-            //Serial.printf("damp %f %f %i\n", x, y, time);
-
-            //float len = vecLen(x-centerX, y-centerY);
-
-            if(x < centerX-deadZone){
-                x = mapf(x, 0.0+outerEdge, centerX-deadZone, -511.0, 0.0);
-            }else if(x > centerX+deadZone){
-                x = mapf(x, centerX+deadZone, 1024.0-outerEdge, 0.0, 511.0);
+            if(v < center-deadZone){
+                v = mapf(v, 0.0+outerEdge, center-deadZone, -range, 0.0);
+            }else if(v > center+deadZone){
+                v = mapf(v, center+deadZone, 1024.0-outerEdge, 0.0, range);
             }else{
-                x = 0.0;
+                v = 0.0;
             }
             
-            if(y < centerY-deadZone){
-                y = mapf(y, 0.0+outerEdge, centerY-deadZone, -511.0, 0.0);
-            }else if(y > centerY+deadZone){
-                y = mapf(y, centerY+deadZone, 1024.0-outerEdge, 0.0, 511.0);
-            }else{
-                y = 0.0;
-            }
-            
-            //Serial.printf("map %f %f %i\n", x, y, time);
+            //Serial.printf("map %f %i\n", v, time);
         };
 };
 

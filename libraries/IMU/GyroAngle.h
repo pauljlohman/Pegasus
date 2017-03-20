db6318ef29141a1da8e5f9bc77ce6fbdab90dd24
Paul Lohman <pauljlohman@hotmail.com>
2017-03-19 23:07:13 -0400

    Bug fix - correction/fusion function
    -fixed issue when either the original or the correction angle were past +-180.

--- libraries/IMU/GyroAngle.h
+++ libraries/IMU/GyroAngle.h
@@ -22,8 +22,10 @@
 
 #ifndef GYROANGLE_H
 #define GYROANGLE_H
-
+#define DEBUG
 class GyroAngle{
+    float radToDegree = 57.295779513082320876798154814105;// 180 / PI
+    float degreeToRad = 0.01745329251994329576923690768489;// PI / 180
     unsigned long dpu; // degrees per time unit
     float deltaDPU;
     float correctionWeight = 0.1;
@@ -42,15 +44,33 @@ class GyroAngle{
         }
         return v;
     };
-
+    
+    float angleDelta(float a, float b){
+        a *= degreeToRad;
+        b *= degreeToRad;
+        float v = atan2f(sinf(a-b), cosf(a-b));
+        return v * radToDegree;     
+    };
+    
+    float angleInterpolate(float a, float b, float w){
+        //http://stackoverflow.com/a/30129248
+        a *= degreeToRad;
+        b *= degreeToRad;
+        float CS = (1.0-w) * cosf(a) + w * cosf(b);
+        float SN = (1.0-w) * sinf(a) + w * sinf(b);
+        float c = atan2f(SN,CS);
+        return c * radToDegree;
+    };
+    
 public:
     // angle rate
     float rateX, rateY, rateZ;
-    // angle rate sum
-    float rsumX, rsumY, rsumZ;
-    // corrected angle
+    // integrated angle rate and corrected using accelerometer
     float posX, posY, posZ;
-
+    #ifdef DEBUG
+        // integrated angle rate
+        float rsumX, rsumY, rsumZ;
+    #endif
     void config(unsigned short FullScaleRangeDPS,
                 unsigned long deltaTime_UnitsPerSecond,
                 float correction_weight,
@@ -82,36 +102,52 @@ public:
         rateX = (x - offsetX) / deltaDPU;
         rateY = (y - offsetY) / deltaDPU;
         rateZ = (z - offsetZ) / deltaDPU;
-        // sum angle using rate only
-        rsumX += rateX;
-        rsumY += rateY;
-        rsumZ += rateZ;
-        rsumX = wrapAngle(rsumX);
-        rsumY = wrapAngle(rsumY);
-        rsumZ = wrapAngle(rsumZ);
-        // current angle plus rate change
+        #ifdef DEBUG
+            // sum angle using rate only
+            rsumX += rateX;
+            rsumY += rateY;
+            rsumZ += rateZ;
+            rsumX = wrapAngle(rsumX);
+            rsumY = wrapAngle(rsumY);
+            rsumZ = wrapAngle(rsumZ);
+        #endif
         posX += rateX;
         posY += rateY;
         posZ += rateZ;
         posX = wrapAngle(posX);
         posY = wrapAngle(posY);
-        posZ = wrapAngle(posZ);
+        posZ = wrapAngle(posZ);        
     };
 
     void correct(float cx, float cy, float cz){
+        /*
         posX = (posX * gyroWeight) + (cx * correctionWeight);
         posY = (posY * gyroWeight) + (cy * correctionWeight);
         posZ = (posZ * gyroWeight) + (cz * correctionWeight);
+        */
+        posX = angleInterpolate(posX, cx, correctionWeight);
+        posY = angleInterpolate(posY, cy, correctionWeight);
+        posZ = angleInterpolate(posZ, cz, correctionWeight);
     };
     void correctXY(float cx, float cy){
+        /*
         posX = (posX * gyroWeight) + (cx * correctionWeight);
         posY = (posY * gyroWeight) + (cy * correctionWeight);
+        */
+        posX = angleInterpolate(posX, cx, correctionWeight);
+        posY = angleInterpolate(posY, cy, correctionWeight);
     };
     void correctZ(float cz){
-        posZ = (posZ * gyroWeight) + (cz * correctionWeight);
+        //posZ = (posZ * gyroWeight) + (cz * correctionWeight);
+        posZ = angleInterpolate(posZ, cz, correctionWeight);
     };
-
-
+    
+    void zeroZ(){
+            posZ = 0.0;
+        #ifdef DEBUG
+            rsumZ = 0.0;
+        #endif
+    };
 };
 
 #endif // GYROANGLE_H
